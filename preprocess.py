1ae72951224a2fd227764d6867b2a4b993c63b89
nikhilNathwani <njn27@cornell.edu>
2013-11-22 00:51:01 -0500

    Added support for smileys so that they don't get lowercased or removed with punctuation

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,3 @@
-#Problem 2
-
 import string
 import sys
 import csv
@@ -16,11 +14,58 @@ trainData= []
 testData= []
 dataSets= {"train": trainData, "test": testData}
 
-smileys= [":)",":(",":-)",":-(", ":D"]
+smileys= [":)",":(",":-)",":-(", ":D",":P",":-D","=)"]
 
+#Remove trailing leading whitespace and turn excessive whitespace to one whitespace
 #Keep smileys in the tweets! (eliminating punctuation gets rid of them!)
 
-## def preprocess2(train,test):
+def preprocess(train,test):
+  inputFile= {"train": train, "test":test}
+  global trainData, testData, dataSets
+  for k,data in enumerate(["train","test"]):
+    csv_file= csv.writer(open(filenames[data], "wb"))
+    with open(inputFile[data]) as f1: 
+      for j,line in enumerate(f1):
+        if len(line)>1 and j<=15:
+          line= line.split('","')
+          for i,elem in enumerate(line):
+            smileSet= sets.Set()
+            for smile in smileys:
+              inds= [(m.start(), len(smile), smile) for m in re.finditer(re.compile(re.escape(smile)), elem)]
+              smileSet= set(smileSet).union(sets.Set(inds))
+            smileSet= sorted(list(smileSet))
+            currString= ""
+            startInd= 0
+            for (index, length, smile) in smileSet:
+              currString += removePunctuation(elem[startInd:index],len(line),i) + smile
+              startInd= index+length
+            currString += removePunctuation(elem[startInd:],len(line),i)
+            line[i]= currString
+          dataSets[data] += line  
+          csv_file.writerow(line)
+
+def removePunctuation(str, lineLen, i):
+  if i==0: str= str.replace("\"", "")
+  if i==lineLen-1: str= str.replace("\n", "").translate(None,'\"')
+  if i<=4: return str.translate(None, string.punctuation).lower()
+  else: return str
+
+
+## def preprocess2(train, test):
+##   global trainData, testData, dataSets
+##   inputFile= {"train": train, "test":test}
+##   for k,data in enumerate(["train","test"]):
+##     with open(inputFile[data]) as f1: 
+##       for line in f1:
+##           line= line.lower()
+##           for elem 
+##           line= line.split('","')
+##           dataSets[data] += line
+##   print dataSets
+      
+
+
+## def preprocess2(train, test):
 ##   inputFile= {"train": train, "test":test}
 ##   global trainData, testData, dataSets
 ##   for k,data in enumerate(["train","test"]):
@@ -30,61 +75,14 @@ smileys= [":)",":(",":-)",":-(", ":D"]
 ##         if len(line)>1:
 ##           line= line.lower()
 ##           line= line.split('","')
-##           ret= []
-##           smileSet= sets.Set()
 ##           for i,elem in enumerate(line):
-##             For smile in smileys:
-##               inds= [(m.start(), len(smile)) for m in re.finditer(re.compile(re.escape(smile)), ":)  :)   :)")]
-##               smileSet= smileSet.union(sets.Set(inds))
-##             smileSet= sorted(list(smileSet))
-##             print smileSet
-##             currString= ""
-##             startInd= 0
-##             for (index, length) in smileSet:
-##               currString += removePunctuation(elem[startInd:index],i)
-              
-
+##             if i==0: elem= elem.replace("\"", "")
+##             if i==len(line)-1: elem= elem.replace("\n", "")
 ##             line[i]= elem.translate(None, string.punctuation)
-##           dataSets[data] += line  
-##           csv_file.writerow(line)
-
-
-def preprocess(train, test):
-  global trainData, testData, dataSets
-  inputFile= {"train": train, "test":test}
-  for k,data in enumerate(["train","test"]):
-    with open(inputFile[data]) as f1: 
-      for line in f1:
-          line= line.lower()
-          line= line.split('","')
-          dataSets[data] += line
-  print dataSets
-      
-
-
-def preprocess3(train, test):
-  inputFile= {"train": train, "test":test}
-  global trainData, testData, dataSets
-  for k,data in enumerate(["train","test"]):
-    csv_file= csv.writer(open(filenames[data], "wb"))
-    with open(inputFile[data]) as f1: 
-      for j,line in enumerate(f1):
-        if len(line)>1:
-          line= line.lower()
-          line= line.split('","')
-          for i,elem in enumerate(line):
-            if i==0: elem= elem.replace("\"", "")
-            if i==len(line)-1: elem= elem.replace("\n", "")
-            line[i]= elem.translate(None, string.punctuation)
-          dataSets[data] += line
-  print dataSets
-
-  
-def removePunctuation(str, i):
-  if i==0: elem= elem.replace("\"", "")
-  if i==len(line)-1: elem= elem.replace("\n", "")
-  return str.translate(None, string.punctuation)
+##           dataSets[data] += line
+##   print dataSets
   
 if __name__ == '__main__':
   print "Starting preprocess"
   preprocess(sys.argv[1],sys.argv[2])
+
