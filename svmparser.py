e325f0b2c9ad2c6aa627946e848b28f47b044132
Aparna Pande <ap539@cornell.edu>
2013-12-09 22:46:15 -0500

    kindsvm changes

--- svmparser.py
+++ svmparser.py
@@ -4,16 +4,20 @@
 
 import numpy as np
 #import pandas as pd
-import cPickle as pickle
+#import cPickle as pickle
 
-infile1 = open('kindWords.txt', 'r') ######################
+filenum = '15'
+elemkind = 27
+
+infile1 = open('Important_Word_Output/kindWords'+filenum, 'r')
 line = infile1.readline().strip()
-line = line[1:-1]
-whenwords = line.split("\', \'")
+whenwords = line.split(", ")
+
 infile2 = open('trainV2')
 alltraindata = [line.strip() for line in infile2]
-svmfile = open('svmfiles/trainsvmkind14', 'w') #####################
-svmvalfile = open('svmfiles/trainsvmkind14val', 'w') #####################
+
+svmfile = open('svmfiles/trainsvmkind'+filenum, 'w')
+svmvalfile = open('svmfiles/trainsvmkind'+filenum+'val', 'w')
 
 when_dict = {}
 index_dict = {}
@@ -32,19 +36,26 @@ def zeroDict():
 	for key in when_dict:
 		when_dict[key] = 0
 
-def buildFeatureVector(tweet):
-	tweetwords = tweet.split(" ")
-	for word in tweetwords:
-		if (word in when_dict):
-			when_dict[word] += 1
-	return (when_dict.values())
-
 def buildsvmline(tweet, classification):
 	tweetwords = tweet.split(" ")
-	if classification == 14: ###############################
+	if classification == 14: 
 		s = "1"
 	else: s = "-1"
 	#s = `classification`
+	for word in tweetwords:
+		if word in when_dict:
+			when_dict[word] += 1
+	for i in range(len(whenwords)):
+		count = when_dict[whenwords[i]]
+		if count > 0:
+			if i != index_dict[whenwords[i]]:
+				print " WHATS THE"
+			s = s + " " + `(i+1)` + ":" + `count`
+	return s
+
+def buildsvmlinekind(tweet, classification):
+	tweetwords = tweet.split(" ")
+	s = `classification`
 	for word in tweetwords:
 		if word in when_dict:
 			when_dict[word] += 1
@@ -62,20 +73,23 @@ def buildsvmline(tweet, classification):
 buildIndexDict()
 buildWhenDict()
 
-
 g = 1
 for k in range(len(alltraindata)):
     trainvaluelist = [i for i in alltraindata[k].split(",")]
-    when = [float(i) for i in trainvaluelist[13:28]] ######################
-    index = when.index(max(when))
+    #when = [float(i) for i in trainvaluelist[13:28]] 
+    #index = when.index(max(when))
+    whenkind = float(trainvaluelist[elemkind]) 
+    if whenkind >= 0.7:
+    	classkind = 1
+    else: classkind = 0
     tweet = trainvaluelist[1]
-    #print tweet, when
-    #vec = buildFeatureVector(tweet)
-    svmstr = buildsvmline(tweet, index)
+    #svmstr = buildsvmline(tweet, index)
+    svmstr = buildsvmlinekind(tweet, classkind)
     if g < 60000:
     	svmfile.write(svmstr + "\n")
     else:
     	svmvalfile.write(svmstr + "\n")
+    g += 1
     zeroDict()
 
 #print vec
